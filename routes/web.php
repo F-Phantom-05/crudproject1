de14552d7ede729757364ce5ba16b087b5faa256
F-Phantom-05 <Jundellesillo001@gmail.com>
2023-05-29 17:27:32 +0800

    add login

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,11 @@ use App\Http\Controllers\CompanyController;
 |
 */
 
+Route::get('login', [RegAccount::class, 'index'])->name('login');
+Route::post('post-login', [RegAccount::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [RegAccount::class, 'registration'])->name('register');
+Route::post('post-registration', [RegAccount::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [RegAccount::class, 'dashboard']); 
+Route::get('logout', [RegAccount::class, 'logout'])->name('logout');
+
 Route::resource('companies', CompanyController::class);
